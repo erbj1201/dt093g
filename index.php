99efb2c16c67f1560d8ebd44f0bc63686eaf7f09
Erika Vestin <erbj1201@student.miun.se>
2023-01-31 12:19:00 +0100

    Testar commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$page_title = "Startsida";
+include("includes/header.php");
+?>
+
+<h2>Startsidan</h2>
+<p>Det här är startsidan.</p>
+
+<?php
+include("includes/sidebar.php");
+include("includes/footer.php");
