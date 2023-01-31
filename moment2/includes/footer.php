ce0c097afdaab45fd4a32c00a60864451dccafb8
Erika Vestin <erbj1201@student.miun.se>
2023-01-31 13:59:32 +0100

    Uppdaterat färger i CSS och text i footer

--- moment2/includes/footer.php
+++ moment2/includes/footer.php
@@ -1,5 +1,7 @@
 <footer id="mainfooter">
-            <p>Här är webbplatsens sidfot. Samma för alla undersidor.</p>
+            <p>DT093G - Webbutveckling II
+            <br>Webbutvecklingsprogrammet, Mittuniversitetet
+            <br> Copyright © Erika Vestin 2023 </p>
         </footer><!-- /mainfooter -->
     </div><!-- /container -->
     <script src="js/main.js"></script>
